36754da59f600d46ef747c15a4c5045c03e23270
Sonadi <144614600+Sonadi@users.noreply.github.com>
2024-12-06 00:11:02 +0530

    connect to database

--- app/src/main/java/com/example/taskmanagement/AddNoteActivity.kt
+++ app/src/main/java/com/example/taskmanagement/AddNoteActivity.kt
@@ -1,20 +1,32 @@
 package com.example.taskmanagement
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.taskmanagement.databinding.ActivityAddNoteBinding
+import com.example.taskmanagement.databinding.ActivityMainBinding
 
 class AddNoteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db:NoteDatabaseHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_add_note)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NoteDatabaseHelper(this)
+
+        binding.savebutton.setOnClickListener{
+            val title = binding.titleedittext.text.toString()
+            val content = binding.contentedittext.text.toString()
+            val note = Note(0,title,content)
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this,"Task saved",Toast.LENGTH_SHORT).show()
+
         }
     }
 }
---
+++ app/src/main/java/com/example/taskmanagement/Note.kt
@@ -0,0 +1,7 @@
+package com.example.taskmanagement
+
+data class Note(
+    val id:Int,
+    val title:String,
+    val content:String
+)
