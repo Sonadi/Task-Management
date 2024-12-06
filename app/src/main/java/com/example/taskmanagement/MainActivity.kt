e08ae0d1944d580569ee95e74714817841c728d3
Sonadi <144614600+Sonadi@users.noreply.github.com>
2024-12-06 18:38:40 +0530

    data

--- app/src/main/java/com/example/taskmanagement/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagement/MainActivity.kt
@@ -6,19 +6,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.taskmanagement.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db : NoteDatabaseHelper
+    private lateinit var noteAdapter: NoteAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        db = NoteDatabaseHelper(this)
+        noteAdapter = NoteAdapter(db.getAllNotes(),this)
+        binding.taskRecycleView.layoutManager = LinearLayoutManager(this)
+        binding.taskRecycleView.adapter = noteAdapter
+
         binding.addbutton.setOnClickListener {
             val intent = Intent(this,AddNoteActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        noteAdapter.refreshData(db.getAllNotes())
     }
 }
